5623160bbc288d70c717a9f2a9c89dc05b10524f
sousk333 <fire.bluestar@gmail.com>
2015-10-31 17:04:30 +0900

    modified. 第2章 書籍に合わせる

--- spec/lib/bowling_game_spec.rb
+++ spec/lib/bowling_game_spec.rb
@@ -5,16 +5,18 @@ describe BowlingGame do
     end
 
     subject do
-      20.times.each { @game.record_shot(pins) }
+      throw_count.times.each { @game.record_shot(pins) }
       @game.score
     end
 
     context "全ての投球がガターの場合" do
+      let(:throw_count) { 20 }
       let(:pins) { 0 }
       it { expect(subject).to eq 0 }
     end
 
     context "全ての投球で1ピンだけ倒した" do
+      let(:throw_count) { 20 }
       let(:pins) { 1 }
       it { expect(subject).to eq 20 }
     end
